4f9057d4668ef100d71e3ead1e959191e625897e
Giordano Salvador <73959795+e3m3@users.noreply.github.com>
2024-09-14 22:23:39 -0400

    Update container build scripts
    *	Fix output for MacOS docker buildx
    
    *	Update root user home directory in containers

--- scripts/build-and-test-macos14-container.sh
+++ scripts/build-and-test-macos14-container.sh
@@ -5,23 +5,17 @@
 HOMEBREW_HOME=/opt/homebrew
 eval "$(${HOMEBREW_HOME}/bin/brew shellenv)"
 
-brew install docker docker-buildx
-
-mkdir -p /root/.docker
-echo '
-  "cliPluginsExtraDirs": [
-      "/opt/homebrew/lib/docker/cli-plugins"
-  ]
-' >> /root/.docker/config.json
+brew install docker-buildx
 
 case ${BUILD_MODE} in
     debug)      build_mode= ;;
     release)    build_mode=--release ;;
-    *)          echo "Error: BUILD_MODE=$BUILD_MODE" >2  &&  exit 1 ;;
+    *)          echo "Error: BUILD_MODE=$BUILD_MODE" >&2  &&  exit 1 ;;
 esac
 
-docker buildx \
+docker-buildx build \
     -f container/Containerfile.${DISTRO}${OS_VER} \
     -t ${OS}-calcc${build_mode} \
     --build-arg=BUILD_MODE=${build_mode} \
+    --progress=plain \
     .
