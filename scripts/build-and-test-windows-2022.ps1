ad23b562b528711b4cfedbb111e9429ff0f1367d
Giordano Salvador <73959795+e3m3@users.noreply.github.com>
2024-09-25 13:04:50 -0400

    Fix Windows 2022 build script

--- scripts/build-and-test-windows-2022.ps1
+++ scripts/build-and-test-windows-2022.ps1
@@ -14,14 +14,17 @@ if ( !$env:WORKSPACE ) {
 }
 
 #   Bootstrap chocolatey
+$CHOCOLATEY_URL="https://community.chocolatey.org/install.ps1"
 Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+[System.Net.ServicePointManager]::SecurityProtocol = `
+    [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString("$CHOCOLATEY_URL"))
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
 
 #   Install dependencies
 choco feature disable --name showDownloadProgress
+choco install -y sed
 choco install -y git.install
 choco install -y ninja
 choco install -y cmake.install
@@ -29,10 +32,15 @@ choco install -y gnuwin32-coreutils.install
 choco install -y python3
 choco install -y python3-virtualenv
 choco install -y StrawberryPerl
-choco install -y psutils --checksum "8B651AC5DFF04BA64468F9ABC78F1A0483EE05CDB51218C63DA98096A5A7F5A0"
+choco install -y psutils `
+    --checksum "8B651AC5DFF04BA64468F9ABC78F1A0483EE05CDB51218C63DA98096A5A7F5A0"
 choco install -y rustup.install
 choco install -y visualstudio2022-workload-vctools
 refreshenv
+
+#   Setup visual studio
+$VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2022"
+cmd /C "$VS_PATH\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" "amd64"
 
 #   Setup python
 $LLVM_VER_LONG="18.1.8"
@@ -44,30 +52,51 @@ $env:PATH="$env:PYTHON_VENV_PATH\bin;$env:PATH"
 pip install "lit==$LLVM_VER_LONG"
 
 #   Setup llvm
-$LLVM_VER="18.x"
+$LLVM_VER_MAJOR="18"
+$LLVM_VER="$LLVM_VER_MAJOR.x"
 $LLVM_SRC="$env:WORKSPACE\llvm-project"
 $env:LLVM_SYS_181_PREFIX="$LLVM_SRC\install"
 $JOBS="4"
+$CMAKE_BUILD_TYPE="MinSizeRel"
 
-git clone --recursive --branch "release/$LLVM_VER" "https://github.com/llvm/llvm-project" "$LLVM_SRC"
+git clone --recursive --branch "release/$LLVM_VER" `
+    "https://github.com/llvm/llvm-project" "$LLVM_SRC"
 cd "$LLVM_SRC"
 cmake `
-    -DCMAKE_BUILD_TYPE="MinSizeRel" `
-    -DCMAKE_INSTALL_PREFIX="$LLVM_SRC\install" `
+    -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" `
+    -DCMAKE_INSTALL_PREFIX="$env:LLVM_SYS_181_PREFIX" `
     -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;libc;lld;polly" `
-    -DLLVM_TARGETS_TO_BUILD="AArch64;X86" `
+    -DLLVM_TARGETS_TO_BUILD="X86" `
+    -DLLVM_LIT_TOOLS="C:\Program Files (x86)\GnuWin32" `
+    -DLLVM_BUILD_TOOLS=ON `
+    -DLLVM_INCLUDE_TOOLS=ON `
+    -DLLVM_INSTALL_UTILS=ON `
     -S "$LLVM_SRC\llvm" `
     -B "$LLVM_SRC\build"
-cmake --build "$LLVM_SRC\build" -j "$JOBS"
-cmake --build "$LLVM_SRC\build" -j "$JOBS" --target install
-$env:PATH="$LLVM_SRC\install\bin;$env:PATH"
-cd "$env:WORKSPACE"
+cmake --build "$LLVM_SRC\build" -j "$JOBS" --config "$CMAKE_BUILD_TYPE"
+
+#   Perform LLVM install manually
+md "$env:LLVM_SYS_181_PREFIX" -ea 0
+cp "$LLVM_SRC\build\$CMAKE_BUILD_TYPE\libllvm-c.args" "$env:LLVM_SYS_181_PREFIX"
+cp "$LLVM_SRC\build\$CMAKE_BUILD_TYPE\libllvm-c.exports" "$env:LLVM_SYS_181_PREFIX"
+cp -r "$LLVM_SRC\build\$CMAKE_BUILD_TYPE\bin" "$env:LLVM_SYS_181_PREFIX"
+cp -r "$LLVM_SRC\build\$CMAKE_BUILD_TYPE\lib" "$env:LLVM_SYS_181_PREFIX"
+cp -r "$LLVM_SRC\build\$CMAKE_BUILD_TYPE\lib\clang\$LLVM_VER_MAJOR\include" `
+    "$env:LLVM_SYS_181_PREFIX"
+cp -r "$LLVM_SRC\build\$CMAKE_BUILD_TYPE\lib\clang\$LLVM_VER_MAJOR\lib" `
+    "$env:LLVM_SYS_181_PREFIX"
+cp -r "$LLVM_SRC\build\$CMAKE_BUILD_TYPE\lib\clang\$LLVM_VER_MAJOR\share" `
+    "$env:LLVM_SYS_181_PREFIX"
+cp -r "$LLVM_SRC\build\include" "$env:LLVM_SYS_181_PREFIX"
+$env:PATH="$env:LLVM_SYS_181_PREFIX\bin;$env:PATH"
 
 #   Setup rust
 $RUSTUP_CHANNEL="stable"
-
 rustup toolchain install "$RUSTUP_CHANNEL"
 rustup override set "$RUSTUP_CHANNEL"
+
+cd "$PSScriptRoot\.."
+sed -i "s/force-dynamic/force-static/g" ".\Cargo.toml"
 
 if ( "$env:BUILD_MODE" -eq "debug" ) {
     cargo build --verbose
